dcc8341d19d174d4d7ac2395c7eed726746cf29f
i7qce <111165003+i7qce@users.noreply.github.com>
2023-01-22 00:59:10 +0000

    tutorial

--- tb/src/main.rs
+++ tb/src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, short = 's')]
+    pattern: String,
+
+    #[arg(long, short = 'p')]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("Looking for {} inside {}", args.pattern, args.path.display());
+
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file!");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
 }
