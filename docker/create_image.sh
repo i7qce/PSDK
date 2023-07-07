434101376059dc06eb6d4ce84e17264ed3e49501
i7qce <111165003+i7qce@users.noreply.github.com>
2023-07-07 19:43:46 +0000

    forgot file in last commit

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,5 +1,7 @@
 # Script to build a docker image from docker file in CWD. Will prompt you for the name of the new image.
 #
+# For this to work, please ensure the terminal is inside the PSDK/docker/ directory when running
+#
 # USAGE: ./create_image.sh
 #
 
